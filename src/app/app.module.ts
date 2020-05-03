69f16fc3634360a8fe28f12c9428d2fb92ffdb65
Bhuvesh Garg <bhuvesh.garg@nagarro.com>
2020-05-03 22:39:02 +0530

    covid app

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CoronaService } from './shared/corona.service';
+import { DistrictComponent } from './district/district.component';
+import { HelpfulLinksComponent } from './helpful-links/helpful-links.component';
+import { FAQComponent } from './faq/faq.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DistrictComponent,
+    HelpfulLinksComponent,
+    FAQComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [CoronaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
